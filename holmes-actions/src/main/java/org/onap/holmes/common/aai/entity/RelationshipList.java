114345035f95a2154fbea29356e33f1b2397fb39
Shiwei Tian <tian.shiwei@zte.com.cn>
2017-09-25 10:33:49 +0800

    add vm data entity and vnf data entity
    Issue-ID: HOLMES-44
    
    Change-Id: I51656e408f9da56bc2141d45c132861fdfcb3c39
    Signed-off-by: Shiwei Tian <tian.shiwei@zte.com.cn>

---
+++ holmes-actions/src/main/java/org/onap/holmes/common/aai/entity/RelationshipList.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.holmes.common.aai.entity;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RelationshipList {
+
+    private List<Relationship> relationships;
+
+    @Setter
+    @Getter
+    public static class Relationship {
+        private String relatedLink;
+        private String relatedTo;
+        private List<RelatedToProperty> relatedToPropertyList;
+        private List<RelationshipData> relationshipDataList;
+    }
+
+    @Setter
+    @Getter
+    public static class RelationshipData {
+        private String relationshipKey;
+        private String relationshipValue;
+    }
+
+    @Getter
+    @Setter
+    public static class RelatedToProperty {
+        private String propertyKey;
+        private String propertyValue;
+    }
+
+}
---
+++ holmes-actions/src/main/java/org/onap/holmes/common/aai/entity/VnfEntity.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.holmes.common.aai.entity;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VnfEntity {
+    private Boolean inMaint;
+    private Boolean closedLoopDisabled;
+    private String orchestrationStatus;
+    private String provStatus;
+    private String resourceVersion;
+    private String serviceId;
+    private String vnfId;
+    private String vnfName;
+    private String vnfType;
+    private RelationshipList relationshipList = new RelationshipList();
+}
