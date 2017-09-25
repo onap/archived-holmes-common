d527d0cbca788f1883fbff1e6579168ca60f5e7d
Shiwei Tian <tian.shiwei@zte.com.cn>
2017-09-25 10:55:13 +0800

    add unit test
    Issue-ID: HOLMES-44
    
    Change-Id: I7b1f8bb895943f7f51a77267a16b878f9f5a294a
    Signed-off-by: Shiwei Tian <tian.shiwei@zte.com.cn>

---
+++ holmes-actions/src/test/java/org/onap/holmes/common/aai/mock/ResponseMock.java
@@ -0,0 +1,164 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.holmes.common.aai.mock;
+
+import java.lang.annotation.Annotation;
+import java.net.URI;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.Link.Builder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+public class ResponseMock extends Response{
+
+    @Override
+    public int getStatus() {
+        return 0;
+    }
+
+    @Override
+    public StatusType getStatusInfo() {
+        return null;
+    }
+
+    @Override
+    public Object getEntity() {
+        return null;
+    }
+
+    @Override
+    public <T> T readEntity(Class<T> aClass) {
+        return null;
+    }
+
+    @Override
+    public <T> T readEntity(GenericType<T> genericType) {
+        return null;
+    }
+
+    @Override
+    public <T> T readEntity(Class<T> aClass, Annotation[] annotations) {
+        return null;
+    }
+
+    @Override
+    public <T> T readEntity(GenericType<T> genericType, Annotation[] annotations) {
+        return null;
+    }
+
+    @Override
+    public boolean hasEntity() {
+        return false;
+    }
+
+    @Override
+    public boolean bufferEntity() {
+        return false;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public MediaType getMediaType() {
+        return null;
+    }
+
+    @Override
+    public Locale getLanguage() {
+        return null;
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+
+    @Override
+    public Set<String> getAllowedMethods() {
+        return null;
+    }
+
+    @Override
+    public Map<String, NewCookie> getCookies() {
+        return null;
+    }
+
+    @Override
+    public EntityTag getEntityTag() {
+        return null;
+    }
+
+    @Override
+    public Date getDate() {
+        return null;
+    }
+
+    @Override
+    public Date getLastModified() {
+        return null;
+    }
+
+    @Override
+    public URI getLocation() {
+        return null;
+    }
+
+    @Override
+    public Set<Link> getLinks() {
+        return null;
+    }
+
+    @Override
+    public boolean hasLink(String s) {
+        return false;
+    }
+
+    @Override
+    public Link getLink(String s) {
+        return null;
+    }
+
+    @Override
+    public Builder getLinkBuilder(String s) {
+        return null;
+    }
+
+    @Override
+    public MultivaluedMap<String, Object> getMetadata() {
+        return null;
+    }
+
+    @Override
+    public MultivaluedMap<String, String> getStringHeaders() {
+        return null;
+    }
+
+    @Override
+    public String getHeaderString(String s) {
+        return null;
+    }
+}
