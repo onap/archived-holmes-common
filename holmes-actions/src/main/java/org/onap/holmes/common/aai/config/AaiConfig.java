55a7b519e1a268408b3912245c9b0ed59daac487
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2019-04-12 10:25:15 +0530

    Update AAI Assistant Tools for CCVPN Extenstion
    Changed the access modifier from public to private
    Changed the password to pwd in a variable
    
    Issue-ID: HOLMES-194
    
    Change-Id: I0517482cc0a6004f12f7225d745a66315d3a2122
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>

--- holmes-actions/src/main/java/org/onap/holmes/common/aai/config/AaiConfig.java
+++ holmes-actions/src/main/java/org/onap/holmes/common/aai/config/AaiConfig.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2017 ZTE Corporation.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -23,12 +23,12 @@ public class AaiConfig {
 
     private static final String AAI_AUTHENTICATION_USER = "AAI";
 
-    private static final String AAI_AUTHENTICATION_PAASWORD = "AAI";
+    private static final String AAI_AUTHENTICATION_PWD = "AAI";
 
     public static String getAuthenticationCredentials() {
-        String usernameAndPassword = AAI_AUTHENTICATION_USER + ":"
-                + AAI_AUTHENTICATION_PAASWORD;
-        return "Basic " + java.util.Base64.getEncoder().encodeToString(usernameAndPassword.getBytes());
+        String userCredentials = AAI_AUTHENTICATION_USER + ":"
+                + AAI_AUTHENTICATION_PWD;
+        return "Basic " + java.util.Base64.getEncoder().encodeToString(userCredentials.getBytes());
     }
 
     public static class AaiConsts {
